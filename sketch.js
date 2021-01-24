2a48610d10b16f87e8c12dad6d378eab03c076b9
Sangeetmoh2006 <74925772+Sangeetmoh2006@users.noreply.github.com>
2021-01-24 15:38:23 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,16 +4,17 @@ const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 
 function setup() {
-	createCanvas(1200, 700);
+	createCanvas(1200, 800);
 
 	engine = Engine.create();
 	world = engine.world;
+	
+	paper1 = new Paper(50,50);
+	ground = new Ground(400, height, 1600, 20);
+	dustbin1 = new Dustbin(900, 690, 100, 100);
+	dustbin2 = new Dustbin(880, 680, 20, 110);
+	dustbin3 = new Dustbin(1000, 680, 20, 110);
 
-	paper1 = new Paper(200,500);
-	dustbin1 = new Dustbin(920,665, 160, 20);
-	ground1 = new Ground(400, 690, 1600, 20);
-	dustbin2 = new Dustbin(830, 600, 20, 110);
-	dustbin3 = new Dustbin(1010, 600, 20, 110);
 
 	Engine.run(engine);
   
@@ -22,17 +23,17 @@ function setup() {
 
 function draw() {
   rectMode(CENTER);
-  background(0);
+  background("white");
   drawSprites();
   paper1.display();
+  ground.display();
   dustbin1.display();
-  ground1.display();
   dustbin2.display();
   dustbin3.display();
 }
 
 function keyPressed() {
 	if (keyCode===UP_ARROW) {
-		Matter.Body.applyForce(paper1.body,paper1.body.position,{x:40, y:-40});
+		Matter.Body.applyForce(paper1.body,paper1.body.position,{x:430, y:-500});
 	}
 }

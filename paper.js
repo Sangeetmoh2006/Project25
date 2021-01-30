24913d8de2351049409a0ba38a6db6fb2084e05e
Sangeetmoh2006 <74925772+Sangeetmoh2006@users.noreply.github.com>
2021-01-30 15:56:50 -0600

    Update paper.js

--- paper.js
+++ paper.js
@@ -2,18 +2,22 @@ class Paper {
     constructor(x, y) {
         var options  = {
             isStatic: false,
-            restitution: 0.5,
-            friction: 0.5,
+            restitution: 0.3,
+            friction: 0,
             density: 1.2
         }
-        this.body = Bodies.circle(x, y, 50, options);
-        this.RADIUS = 50;
+        this.body = Bodies.circle(x, y, 70, options);
+        this.RADIUS = 70;
         this.image = loadImage("sprites/paper.png");
         World.add(world, this.body)
     }
     display() {
         var pos = this.body.position
+        push();
+        translate(pos.x, pos.y);
+        rotate(this.body.angle);
         ellipseMode(RADIUS);
-        image(this.image, pos.x, pos.y, 50);
+        image(this.image, 0, 0, 70, 70);
+        pop();
     }
 }
